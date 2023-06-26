0418d132c719fb241937d4cff9c16e3a405a7001
TigasMoro <122630395+TigasMoro@users.noreply.github.com>
2023-06-26 13:22:49 -0300

    Add files via upload

---
+++ SQL/peso.sql
@@ -0,0 +1,9 @@
+/*A equipe de produção precisa do nome de todas as peças que pesam mais que 500kg, mas não mais que 700kg para inspeção.*/
+SELECT NAME
+FROM Production.product
+WHERE Weight > 500 and  Weight < 700
+
+/* Resposta: HL Road Front Wheel
+             LL Crankset
+		     ML Crankset	 
+			 HL Crankset
